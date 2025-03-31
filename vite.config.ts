57bce8b8e835bfed91e4e607257e00393724dde5
Sergio Monjaraz <mta.sergio.monjaraz@gmail.com>
2025-03-31 02:25:44 -0600

    Refactor Vite configuration by removing unnecessary build options and simplifying the setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist', // Output directory for the build
-    sourcemap: true, // Generate source maps for debugging
-    rollupOptions: {
-      input: {
-        main: 'index.html', // Entry point for the application
-      },
-    },
   },
   server: {
     port: 3000, // Development server port
