8cc4e3c5644a74aa1d7a35f721932ca8fda5a3ad
Sergio Monjaraz <mta.sergio.monjaraz@gmail.com>
2025-06-04 13:29:37 -0600

    Add Rocket 3D model and integrate RocketCanvas component

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -2,5 +2,6 @@ import StarsCanvas from "./Stars";
 import Loader from "./Loader";
 import BallCanvas from "./Ball";  
 import EarthCanvas from "./Earth";
+import RocketCanvas from "./RocketCanvas";
 
-export {  StarsCanvas, Loader, BallCanvas, EarthCanvas };
+export {  StarsCanvas, Loader, BallCanvas, EarthCanvas, RocketCanvas };
